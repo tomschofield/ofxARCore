3045b48967788881ffa9c6497dd52384fbddd0b5
Tom Schofield <tomschofieldart@gmail.com>
2018-01-11 09:14:15 +0000

    Update ofxARCore.cpp
    Included updated ofxAndroid calls from ofxAndroidCheckPermission(CAMERA) to ofxAndroidCheckCameraPermission to keep up with latests ofxAndroid version

--- src/ofxARCore.cpp
+++ src/ofxARCore.cpp
@@ -63,9 +63,9 @@ ofxARCore::~ofxARCore(){
 void ofxARCore::setup(){
     _sessionInitialized = false;
 
-    ofxAndroidRequestPermission(CAMERA);
     // Request camera permissions
-    if(ofxAndroidCheckPermission(CAMERA)) {
+    ofxAndroidRequestCameraPermission();
+    if(ofxAndroidCheckCameraPermission()) {
         setupSession();
     }
 
@@ -148,7 +148,7 @@ void ofxARCore::resumeApp(){
 
     // Check if permissions have been granted, and the session isn't setup yet
     if(!_sessionInitialized){
-        if(ofxAndroidCheckPermission(CAMERA)) {
+        if(ofxAndroidCheckCameraPermission()) {
             setupSession();
         }
     }
